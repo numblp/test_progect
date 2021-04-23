fbfae2a8432b625062bf1b30ca22918ff3db49f6
Vital <vitioklok@gmail.com>
2021-04-23 14:47:30 +0300

    First my commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
-let ischecked = true,
-    derWidth = false;
- 
-console.log(ischecked || derWidth);
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('НАсколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('НАсколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
